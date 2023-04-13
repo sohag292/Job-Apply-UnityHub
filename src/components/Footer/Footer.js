246f3e282b6e2df9160995985982f7214ac4456c
sohag292 <sohagshah571@gmail.com>
2023-04-14 05:49:46 +0600

    Footer Problem solve and final work is done my project is complated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className='bg-dark'>
             <footer className='py-5'>
                 <div className='container'>
-                    <div className='row'>
+                    <div className='row common'>
                         <div className='col-lg-4'>
                             <h3 className='text-white'>UnityHub</h3>
                             <p className='text-light'>There are many variations of passages of Lorem Ipsum, but the majority have suffered alteration in some form.</p>
@@ -20,25 +20,27 @@ export default function Footer() {
                             <p className='text-light'>Careers</p>
                         </div>
                         <div className='col-lg-2 col-md-4 col-sm-6'>
-                            <h6 className='text-light'>Company</h6>
-                            <p className='text-light'>About Us</p>
-                            <p className='text-light'>Work</p>
-                            <p className='text-light'>Latest News</p>
-                            <p className='text-light'>Careers</p>
+                            <h6 className='text-light'>Product</h6>
+                            <p className='text-light'>Prototype</p>
+                            <p className='text-light'>Plans & Pricing</p>
+                            <p className='text-light'>Customers</p>
+                            <p className='text-light'>Integrations</p>
+                            
+
+
                         </div>
                         <div className='col-lg-2 col-md-4 col-sm-6'>
-                            <h6 className='text-light'>Company</h6>
-                            <p className='text-light'>About Us</p>
-                            <p className='text-light'>Work</p>
-                            <p className='text-light'>Latest News</p>
-                            <p className='text-light'>Careers</p>
+                            <h6 className='text-light'>Support</h6>
+                            <p className='text-light'>Help Desk Us</p>
+                            <p className='text-light'>Sales</p>
+                            <p className='text-light'>Become a Partner</p>
+                            <p className='text-light'>Developers</p>
                         </div>
                         <div className='col-lg-2 col-md-4 col-sm-6'>
-                            <h6 className='text-light'>Company</h6>
-                            <p className='text-light'>About Us</p>
-                            <p className='text-light'>Work</p>
-                            <p className='text-light'>Latest News</p>
-                            <p className='text-light'>Careers</p>
+                            <h6 className='text-light'>Contact</h6>
+                            <p className='text-light'> 524 Broadway , NYC Us</p>
+                            <p className='text-light'>+1 777-978-5570</p>
+                           
                         </div>
                     </div>
                     <hr className='gray-hr' />
